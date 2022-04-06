71efff671334788198a00d3e63c4056adf23547d
Pedroosorio97 <pedromrnosorio97@gmail.com>
2022-04-06 15:14:05 +0100

    Changed CountTo10 method by invoking CountToN in it

--- MyFirstMethod/Program.cs
+++ MyFirstMethod/Program.cs
@@ -8,14 +8,12 @@ namespace MyFirstMethod
         {
             CountToN(2);
             CountToN(5);
+            CountTo10();
         }
 
         private static void CountTo10()
         {
-            for (int i = 1; i <= 10; i++)
-            {
-                Console.WriteLine(i);
-            }
+            CountToN(10);
 
 
         }
