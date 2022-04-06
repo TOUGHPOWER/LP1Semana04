272a0589ea82eb02466243fb098a298066b1138a
Pedroosorio97 <pedromrnosorio97@gmail.com>
2022-04-06 15:20:20 +0100

    Add XML apropriate comments to each method on program

--- MyFirstMethod/Program.cs
+++ MyFirstMethod/Program.cs
@@ -4,6 +4,10 @@ namespace MyFirstMethod
 {
     class Program
     {
+        /// <summary>
+        /// Method that runs the program class
+        /// </summary>
+        /// <param name="args"></param>
         static void Main(string[] args)
         {
             CountToN(2);
@@ -11,6 +15,9 @@ namespace MyFirstMethod
             CountTo10();
         }
 
+        /// <summary>
+        /// Counts and prints the numbers 1-10
+        /// </summary>
         private static void CountTo10()
         {
             CountToN(10);
@@ -18,6 +25,10 @@ namespace MyFirstMethod
 
         }
 
+        /// <summary>
+        /// Counts and prints the numbers 1-n
+        /// </summary>
+        /// <param name="n">Max number the counter goes to</param>
         private static void CountToN(int n)
         {
             for (int i = 1; i <= n; i++)
