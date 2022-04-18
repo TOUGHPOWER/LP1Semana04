498797771a68d9d8587a1cf74ea94c271f11f375
Pedroosorio97 <pedromrnosorio97@gmail.com>
2022-04-18 15:34:41 +0100

    Working fibonacci number shower

--- Fibonacci/Program.cs
+++ Fibonacci/Program.cs
@@ -6,13 +6,27 @@ namespace Fibonacci
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Welcome to Fibonacci Number Shower\nChoose a number to show the respective Fibonacci Number");
+            int answer = int.Parse(Console.ReadLine());
+            Console.WriteLine(Fibonacci(answer));
             
         }
 
+        
         private static int Fibonacci(int n)
         {
+            int theFibonacciNumber = 0;
+            if((n==1) || (n == 2))
+            {
+                theFibonacciNumber = 1;
+                return theFibonacciNumber;
+            }
+            else 
+            {
+                theFibonacciNumber = Fibonacci(n-1) + Fibonacci(n-2);
+                return theFibonacciNumber;
+            }
             
-            return n;
         }
     }
 }
